72d32128be84bcccc73cb60dae6f653e1e6569fd
Sam Blackburn <sam.blackburn@red-gate.com>
2017-11-27 16:21:43 +0000

    Need the branch parameter as well

--- .build/_init.ps1
+++ .build/_init.ps1
@@ -10,13 +10,14 @@ function global:build {
     else { Write-Error Build failed. }
 }
 
-function global:merge ([string] $GithubApiToken, [string] $revision) {
+function global:merge ([string] $GithubApiToken, [string]$branch, [string] $revision) {
     if (-not $GithubApiToken) { throw "API token needed" }
+    if (-not $branch) { throw "branch needed" }
     if (-not $revision) { throw "revision needed" }
 
     $base64token = [System.Convert]::ToBase64String([char[]]$GithubApiToken);
     $headers = @{ Authorization="Basic $base64token" };
-    $pulls = Invoke-RestMethod -Headers $headers -Uri "https://api.github.com/search/issues?q=user%3A$owner+repo%3A$repo+type%3Apr+state%3Aopen+label%3Amerge-when-green+head%3A$revision"
+    $pulls = Invoke-RestMethod -Headers $headers -Uri "https://api.github.com/search/issues?q=user%3A$owner+repo%3A$repo+type%3Apr+state%3Aopen+label%3Amerge-when-green+head%3A$branch"
     if ($pulls.total_count -lt 1) { Write-Host "No PRs for this revision. Bye!"; return; }
     $pull = $pulls.items | Select -First 1
     $pullNumber = $pull.number
