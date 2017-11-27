0392bb0433585baa34146a8f47b8d611b8fa34b8
Sam Blackburn <sam.blackburn@red-gate.com>
2017-11-27 16:12:43 +0000

    Parameters instead of constants

--- .build/_init.ps1
+++ .build/_init.ps1
@@ -1,4 +1,7 @@
-Write-Host This is the auto-merge repo.  Here are the available commands:
+$owner = "samblackburn"
+$repo = "auto-merge"
+
+Write-Host "This is the $repo repo.  Here are the available commands:"
 Write-Host `t build -Fore Cyan
 Write-Host `t merge -Fore Cyan
 
@@ -7,18 +10,14 @@ function global:build {
     else { Write-Error Build failed. }
 }
 
-function global:merge ([string] $GithubApiToken) {
+function global:merge ([string] $GithubApiToken, [string] $revision) {
     if (-not $GithubApiToken) { throw "API token needed" }
-
-    $owner = "samblackburn"
-    $repo = "auto-merge"
-    $branch = "add-readme"
-
+    if (-not $revision) { throw "revision needed" }
 
     $base64token = [System.Convert]::ToBase64String([char[]]$GithubApiToken);
     $headers = @{ Authorization="Basic $base64token" };
-    $pulls = Invoke-RestMethod -Headers $headers -Uri "https://api.github.com/search/issues?q=user%3A$owner+repo%3A$repo+type%3Apr+state%3Aopen+label%3Amerge-when-green+head%3A$branch"
-    if ($pulls.total_count -lt 1) { Write-Host "No PRs for this branch. Bye!"; return; }
+    $pulls = Invoke-RestMethod -Headers $headers -Uri "https://api.github.com/search/issues?q=user%3A$owner+repo%3A$repo+type%3Apr+state%3Aopen+label%3Amerge-when-green+head%3A$revision"
+    if ($pulls.total_count -lt 1) { Write-Host "No PRs for this revision. Bye!"; return; }
     $pull = $pulls.items | Select -First 1
     $pullNumber = $pull.number
     $pullUrl = $pull.html_url
